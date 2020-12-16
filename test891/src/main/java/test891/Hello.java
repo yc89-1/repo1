69edfc7bb1d054f31b113e6c4fa95d08da382447
ly@qq.com <ly@qq.com>
2020-12-16 15:08:58 +0800

    提交 git 的常用命令

--- test891/src/main/java/test891/Hello.java
+++ test891/src/main/java/test891/Hello.java
@@ -1,5 +1,19 @@
 package test891;
 
+/**
+ * 
+ * git clone 命令 ==> 生成本地仓库
+ * 
+ * 拷贝工程到本地仓库
+ * 
+ * git commit (保存到本地仓库)   + git push (保存到远程仓库)  (eclipse)
+ * 
+ * git pull 从远程仓库拉代码 (eclipse)
+ * 
+ * 
+ *
+ */
+
 public class Hello {
 	
 	public static void main(String[] args) {
